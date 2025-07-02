0b785e62df568ec6b097bc583d17661778ba433c
noiredex <thevertaurore@gmail.com>
2025-07-02 12:06:44 +0900

    main에 edit 삽입

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Root, {
     loader as rootLoader,
 action as rootAction,
  } from "./routes/root";
+import EditContact from "./routes/edit";
 
 import {
   createBrowserRouter,
@@ -28,6 +29,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+        {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],

91b06efdaf8bac659e06f8297858e7a8f6a7e228
noiredex <thevertaurore@gmail.com>
2025-07-02 13:58:43 +0900

    Form ??  헤깔리는 부분 이해했음. 찾고 수정

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,10 @@
-import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { 
+    Outlet, 
+    Link, 
+    useLoaderData, 
+    Form 
+} from "react-router-dom";
+
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -31,9 +37,9 @@ export default function Root() {
               aria-live="polite"
             ></div>
           </form>
-          <form method="post">
+           <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
             {contacts.length ? (
